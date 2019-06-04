b7d2d9e9ca6095c77f5d72a6902f09564b329f71
wouerner <wouerner@protonmail.com>
2019-06-04 08:33:56 -0300

    novas

---
+++ 2143.php
@@ -0,0 +1,14 @@
+What is the output of the following code?
+
+<?php
+$string = 'Good luck!';
+$start = 10;
+var_dump(substr($string, $start));
+
+var_dump(substr($string, 11));
+?>
+
+A: string(0) ""
+B: bool(false)
+C: string(l) "!"
+D: string(2) "k!"
---
+++ 3008.php
@@ -0,0 +1,13 @@
+What the code will output?
+<?php
+$f = function() {
+    echo "func";
+};
+
+echo gettype($f);
+?>
+
+A) function
+B) string
+C) array
+D) object
